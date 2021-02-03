4ac1eecf928b47bd63bec9b0f342db5c47c74321
floraldenis <floraldenis@gmail.com>
2021-02-03 13:50:08 +0200

    Added Entity super type

---
+++ Mediaverse.Domain/Common/Entity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Mediaverse.Domain.Common
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        public Guid Id { get; }
+
+        protected Entity(Guid id)
+        {
+            if (id == default)
+            {
+                throw new ArgumentException("Given ID is invalid");
+            }
+
+            Id = id;
+        }
+
+        public bool Equals(Entity other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Entity other)
+            {
+                return Equals(other);
+            }
+            
+            return false;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
+
+        public override string ToString() => $"{Id.ToString()}";
+    }
+}
