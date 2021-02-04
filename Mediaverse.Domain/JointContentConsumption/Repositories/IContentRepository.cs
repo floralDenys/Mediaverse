9853e351ef37a4874b0b355a81a97c66a84dfe76
floraldenis <floraldenis@gmail.com>
2021-02-04 13:51:49 +0200

    Added basic part ContentRepository

---
+++ Mediaverse.Domain/JointContentConsumption/Repositories/IContentRepository.cs
@@ -0,0 +1,10 @@
+﻿using Mediaverse.Domain.JointContentConsumption.Entities;
+using Mediaverse.Domain.JointContentConsumption.Enums;
+
+namespace Mediaverse.Domain.JointContentConsumption.Repositories
+{
+    public interface IContentRepository
+    {
+        Content SearchForContent(MediaContentSource source, string queryString);
+    }
+}
---
+++ Mediaverse.Infrastructure/JoinContentConsumption/Repositories/ContentRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Mediaverse.Domain.JointContentConsumption.Entities;
+using Mediaverse.Domain.JointContentConsumption.Enums;
+using Mediaverse.Domain.JointContentConsumption.Repositories;
+using Mediaverse.Infrastructure.Common.Enums;
+using Mediaverse.Infrastructure.Common.Services;
+using Mediaverse.Infrastructure.YouTube;
+
+namespace Mediaverse.Infrastructure.JoinContentConsumption.Repositories
+{
+    public class ContentRepository : IContentRepository
+    {
+        private readonly IQueryStringProcessor _queryStringProcessor;
+        private readonly YouTubeRepository _youTubeRepository;
+
+        public ContentRepository(
+            IQueryStringProcessor queryStringProcessor,
+            YouTubeRepository youTubeRepository)
+        {
+            _queryStringProcessor = queryStringProcessor;
+            _youTubeRepository = youTubeRepository;
+        }
+        
+        public Content SearchForContent(MediaContentSource source, string queryString)
+        {
+            try
+            {
+                var queryStringType = _queryStringProcessor.DefineQueryStringType(source, queryString);
+                
+                switch (source)
+                {
+                    case MediaContentSource.YouTube:
+                        if (queryStringType == QueryStringType.Keywords)
+                        {
+                            _youTubeRepository.SearchForVideosByKeywords(queryString);
+                        }
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(MediaContentSource));
+                }
+                
+                throw new NotImplementedException();
+            }
+            catch (Exception exception)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
