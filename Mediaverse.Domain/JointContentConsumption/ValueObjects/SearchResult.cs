0611416386be3e692051ff41ac823390ea5ba5d8
floraldenis <floraldenis@gmail.com>
2021-02-04 13:51:28 +0200

    Added SearchResult

---
+++ Mediaverse.Domain/JointContentConsumption/ValueObjects/SearchResult.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Mediaverse.Domain.JointContentConsumption.Entities;
+
+namespace Mediaverse.Domain.JointContentConsumption.ValueObjects
+{
+    public class SearchResult
+    {
+        public Content SpecificContent { get; }
+        public IList<Content> ContentList { get; }
+
+        public SearchResult(
+            Content specificContent = null,
+            IList<Content> contentList = null)
+        {
+            SpecificContent = specificContent;
+            ContentList = contentList;
+        }
+    }
+}
