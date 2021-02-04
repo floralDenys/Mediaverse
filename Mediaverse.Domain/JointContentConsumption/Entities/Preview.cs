e629bd1cded0831a9064a89220f59e698dff0086
floraldenis <floraldenis@gmail.com>
2021-02-04 13:51:16 +0200

    Added Thumbnail.cs and Preview

---
+++ Mediaverse.Domain/JointContentConsumption/Entities/Preview.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Mediaverse.Domain.JointContentConsumption.ValueObjects;
+
+namespace Mediaverse.Domain.JointContentConsumption.Entities
+{
+    public class Preview : Content
+    {
+        public Thumbnail Thumbnail { get; }
+        
+        public Preview(Guid videoId, string title, string description, Thumbnail thumbnail)
+            : base(videoId, title, description)
+        {
+            try
+            {
+                Thumbnail = thumbnail ?? throw new ArgumentNullException(nameof(thumbnail));
+            }
+            catch (Exception exception)
+            {
+                throw new InvalidOperationException("Could not create preview", exception); 
+            }
+        }
+    }
+}
---
+++ Mediaverse.Domain/JointContentConsumption/ValueObjects/Thumbnail.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Mediaverse.Domain.JointContentConsumption.ValueObjects
+{
+    public class Thumbnail
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public string Url { get; }
+
+        public Thumbnail(int width, int height, string url)
+        {
+            try
+            {
+                if (width <= 0)
+                {
+                    throw new ArgumentException("Width must be more than 0");
+                }
+
+                if (height <= 0)
+                {
+                    throw new ArgumentException("Height must be more than 0");
+                }
+
+                if (string.IsNullOrEmpty(url))
+                {
+                    throw new ArgumentException("Thumbnail requires an url to an image");
+                }
+
+                Width = width;
+                Height = height;
+                Url = url;
+            }
+            catch (Exception exception)
+            {
+                throw new InvalidOperationException("Could not create thumbnail", exception);
+            }
+        }
+    }
+}
