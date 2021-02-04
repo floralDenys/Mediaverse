cf51c531a7b5d27c3b9c196a9cdf4a0fac52214c
floraldenis <floraldenis@gmail.com>
2021-02-04 13:47:14 +0200

    Added QueryStringProcessor

---
+++ Mediaverse.Infrastructure/Common/Services/IQueryStringProcessor.cs
@@ -0,0 +1,10 @@
+﻿using Mediaverse.Domain.JointContentConsumption.Enums;
+using Mediaverse.Infrastructure.Common.Enums;
+
+namespace Mediaverse.Infrastructure.Common.Services
+{
+    public interface IQueryStringProcessor
+    {
+        QueryStringType DefineQueryStringType(MediaContentSource source, string queryString);
+    }
+}
---
+++ Mediaverse.Infrastructure/Common/Services/Implementation/QueryStringProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Mediaverse.Domain.JointContentConsumption.Enums;
+using Mediaverse.Infrastructure.Common.Enums;
+
+namespace Mediaverse.Infrastructure.Common.Services.Implementation
+{
+    public class QueryStringProcessor : IQueryStringProcessor
+    {
+        private readonly IDictionary<MediaContentSource, string> MediaContentSourceDomains =
+            new Dictionary<MediaContentSource, string>
+            {
+                { MediaContentSource.YouTube, "youtube.com/watch"}
+            };
+
+        public QueryStringType DefineQueryStringType(MediaContentSource source, string queryString)
+        {
+            if (!MediaContentSourceDomains.Keys.Contains(source))
+            {
+                throw new InvalidOperationException("Request domain is not specified for selected content source");
+            }
+
+            string selectedSourceDomain = MediaContentSourceDomains[source];
+            return queryString.Contains(selectedSourceDomain)
+                ? QueryStringType.Link
+                : QueryStringType.Keywords;
+        }
+    }
+}
