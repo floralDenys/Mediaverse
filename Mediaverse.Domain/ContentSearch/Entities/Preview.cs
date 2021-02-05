9e1511c859e29a4cbae6b9f42ed611723bb62461
floraldenis <floraldenis@gmail.com>
2021-02-05 11:36:17 +0200

    Updated Preview

--- Mediaverse.Domain/ContentSearch/Entities/Preview.cs
+++ Mediaverse.Domain/ContentSearch/Entities/Preview.cs
@@ -1,19 +1,40 @@
 ﻿using System;
+using Mediaverse.Domain.ContentSearch.Enums;
 using Mediaverse.Domain.ContentSearch.ValueObjects;
-using Mediaverse.Domain.JointContentConsumption.Entities;
-using Mediaverse.Domain.JointContentConsumption.ValueObjects;
 
 namespace Mediaverse.Domain.ContentSearch.Entities
 {
-    public class Preview : Content
+    public class Preview
     {
-        public Thumbnail Thumbnail { get; }
+        public Guid ContentId { get; }
+        public ContentType ContentType { get; }
+        public string ContentTitle { get; }
+        public string ContentDescription { get; }
+        public Thumbnail Thumbnail { get; set; }
         
-        public Preview(Guid videoId, string title, string description, Thumbnail thumbnail)
-            : base(videoId, title, description)
+        public Preview(
+            Guid contentId,
+            ContentType contentType,
+            string contentTitle,
+            string contentDescription,
+            Thumbnail thumbnail)
         {
             try
             {
+                if (contentId == default)
+                {
+                    throw new ArgumentException("Given ID is invalid");    
+                }
+
+                if (string.IsNullOrEmpty(contentTitle))
+                {
+                    throw new ArgumentException("Given title is null or empty");
+                }
+
+                ContentId = contentId;
+                ContentType = contentType;
+                ContentTitle = contentTitle;
+                ContentDescription = contentDescription;
                 Thumbnail = thumbnail ?? throw new ArgumentNullException(nameof(thumbnail));
             }
             catch (Exception exception)
