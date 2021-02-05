67b47a3bebe6cb76583654a3eb3af0a7a2faed85
floraldenis <floraldenis@gmail.com>
2021-02-05 11:32:36 +0200

    Moved YouTubeRepository to Mediaverse.Infrastructure

---
+++ Mediaverse.Infrastructure/ContentSearch/Repositories/YouTube/YouTubeRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Google.Apis.YouTube.v3;
+using Mediaverse.Domain.ContentSearch.ValueObjects;
+
+namespace Mediaverse.Infrastructure.ContentSearch.Repositories.YouTube
+{
+    public class YouTubeRepository
+    {
+        private readonly YouTubeService _youTubeService;
+        private readonly IMapper _mapper;
+
+        private const string SearchParts = "id,snippet";
+        private const string SearchType = "video";
+        private const int MaxSearchResultQuantity = 25;
+
+        public YouTubeRepository(
+            YouTubeService youTubeService,
+            IMapper mapper)
+        {
+            _youTubeService = youTubeService;
+            _mapper = mapper;
+        }
+        
+        public async Task<SearchResult> SearchForVideosByKeywords(string keywords)
+        {
+            var searchRequest = _youTubeService.Search.List(SearchParts);
+            searchRequest.Q = keywords;
+            searchRequest.Type = SearchType;
+            searchRequest.MaxResults = MaxSearchResultQuantity;
+            searchRequest.VideoEmbeddable = SearchResource.ListRequest.VideoEmbeddableEnum.True__;
+
+            var searchResult = await searchRequest.ExecuteAsync();
+            return _mapper.Map<SearchResult>(searchResult);
+        }
+
+        public async Task<SearchResult> SearchForVideoById(string videoId)
+        {
+            var searchRequest = _youTubeService.Videos.List(SearchParts);
+            searchRequest.Id = videoId;
+
+            var searchResult = await searchRequest.ExecuteAsync();
+            return _mapper.Map<SearchResult>(searchResult);
+        }
+    }
+}
