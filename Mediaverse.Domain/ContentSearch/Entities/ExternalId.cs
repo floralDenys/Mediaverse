b21b24e4c786533ee4c0e7dd3bea056a23dbd57b
floraldenis <floraldenis@gmail.com>
2021-02-05 17:20:23 +0200

    Added ExternalId and moved Preview to ValueObjects

---
+++ Mediaverse.Domain/ContentSearch/Entities/ExternalId.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Mediaverse.Domain.ContentSearch.Enums;
+
+namespace Mediaverse.Domain.ContentSearch.Entities
+{
+    public class ExternalId
+    {
+        public string Id { get; }
+        public MediaContentSource ContentSource { get; }
+        public ContentType ContentType { get; }
+
+        public ExternalId(
+            string id,
+            MediaContentSource contentSource,
+            ContentType contentType)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(id))
+                {
+                    throw new ArgumentException("External ID is null or empty");
+                }
+                
+                Id = id;
+                ContentSource = contentSource;
+                ContentType = contentType;
+            }
+            catch (Exception exception)
+            {
+                throw new InvalidOperationException("Could not create external ID", exception);
+            }
+        }
+    }
+}
