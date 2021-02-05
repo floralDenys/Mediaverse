5f475d0f145bf567a4f8192151612fa28459b25a
floraldenis <floraldenis@gmail.com>
2021-02-05 11:34:29 +0200

    Added ContentIdProvider and IContentIdRepository

---
+++ Mediaverse.Infrastructure/Common/Repositories/IContentIdRepository.cs
@@ -0,0 +1,11 @@
+﻿using Mediaverse.Domain.ContentSearch.Enums;
+using Mediaverse.Infrastructure.Common.Services.Implementation;
+
+namespace Mediaverse.Infrastructure.Common.Repositories
+{
+    public interface IContentIdRepository
+    {
+        ContentIdProvider.ContentId GetContentId(string externalId, MediaContentSource source);
+        void SaveContentId(ContentIdProvider.ContentId contentId);
+    }
+}
---
+++ Mediaverse.Infrastructure/Common/Services/IContentIdProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Mediaverse.Domain.ContentSearch.Enums;
+
+namespace Mediaverse.Infrastructure.Common.Services
+{
+    public interface IContentIdProvider
+    {
+        Guid GetOrCreateInternalId(string externalId, MediaContentSource source);
+    }
+}
