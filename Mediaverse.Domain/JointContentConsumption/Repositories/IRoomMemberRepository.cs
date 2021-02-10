21267bec08f39ad06a510d4099ee4719d14ce665
floraldenis <floraldenis@gmail.com>
2021-02-10 22:17:56 +0200

    Added IRoomMemberRepository

---
+++ Mediaverse.Domain/JointContentConsumption/Repositories/IRoomMemberRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Mediaverse.Domain.JointContentConsumption.ValueObjects;
+
+namespace Mediaverse.Domain.JointContentConsumption.Repositories
+{
+    public interface IRoomMemberRepository
+    {
+        Task<Host> GetHostAsync(Guid memberId, CancellationToken cancellationToken);
+    }
+}
