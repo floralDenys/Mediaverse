6c43981e5898e749281bfb99ec37257ee85e7aac
floraldenis <floraldenis@gmail.com>
2021-02-10 22:18:43 +0200

    Added CreateRoomCommand and Handler

---
+++ Mediaverse.Application/JointContentConsumption/Commands/CreateRoom/CreateRoomCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MediatR;
+using Mediaverse.Application.JointContentConsumption.Commands.CreateRoom.Dtos;
+
+namespace Mediaverse.Application.JointContentConsumption.Commands.CreateRoom
+{
+    public class CreateRoomCommand : IRequest<RoomDto>
+    {
+        public string Name { get; set; }
+        public Guid HostId { get; set; }
+    }
+}
