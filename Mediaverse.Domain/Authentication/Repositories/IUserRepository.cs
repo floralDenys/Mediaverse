15a6e39c0762686a110dc5f095c399535652f5e6
floraldenis <floraldenis@gmail.com>
2021-02-13 20:36:50 +0200

    Added IUserRepository

---
+++ Mediaverse.Domain/Authentication/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Mediaverse.Domain.Authentication.Entities;
+
+namespace Mediaverse.Domain.Authentication.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserAsync(Guid userId);
+        Task SaveUserAsync(User user);
+    }
+}
