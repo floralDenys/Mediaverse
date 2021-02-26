2b8709e6483d2eb2e986ec1ea8cfbee79a0f2c53
floraldenis <floraldenis@gmail.com>
2021-02-26 21:29:41 +0200

    Updated ApplicationDbContext

--- Mediaverse.Infrastructure/Common/Persistence/ApplicationDbContext.cs
+++ Mediaverse.Infrastructure/Common/Persistence/ApplicationDbContext.cs
@@ -1,9 +1,38 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mediaverse.Domain.Authentication.Entities;
+using Mediaverse.Domain.JointContentConsumption.Entities;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
 
 namespace Mediaverse.Infrastructure.Common.Persistence
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Playlist> Playlists { get; set; }
+        public DbSet<Room> Rooms { get; set; }
         
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-9G78RAB\\SQLEXPRESS;Integrated Security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+        }
     }
 }
