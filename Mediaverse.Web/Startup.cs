3fa8d8a0f98fa6b1b497601d510fed0eb24f2651
floraldenis <floraldenis@gmail.com>
2021-04-11 12:07:40 +0300

    Added services injection

--- Mediaverse.Web/Startup.cs
+++ Mediaverse.Web/Startup.cs
@@ -1,7 +1,12 @@
 using System.Reflection;
 using MediatR;
+using Mediaverse.Application.Authentication.Services;
+using Mediaverse.Application.Common.Services;
 using Mediaverse.Domain.Authentication.Repositories;
 using Mediaverse.Infrastructure.Authentication.Repositories;
+using Mediaverse.Infrastructure.Authentication.Services;
+using Mediaverse.Infrastructure.Common.Persistence;
+using Mediaverse.Infrastructure.Common.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -26,8 +31,14 @@ namespace Mediaverse.Web
                 .AddRazorRuntimeCompilation();
             
             services.AddMediatR(Assembly.Load("Mediaverse.Application"));
+            services.AddAutoMapper(Assembly.Load("Mediaverse.Application"));
+            services.AddAutoMapper(Assembly.Load("Mediaverse.Infrastructure"));
+            services.AddLogging();
 
+            services.AddDbContext<ApplicationDbContext>();
             services.AddScoped(typeof(IUserRepository), typeof(UserRepository));
+            services.AddScoped(typeof(IGuidProvider), typeof(DefaultGuidProvider));
+            services.AddScoped(typeof(INameGenerator), typeof(NameGenerator));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
