9256cb1d0d961a7864bec7d8bf11d270201ef423
floraldenis <floraldenis@gmail.com>
2021-04-11 21:34:18 +0300

    Added repository dtos

---
+++ Mediaverse.Infrastructure/JointContentConsumption/Repositories/Dtos/RoomDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mediaverse.Infrastructure.JointContentConsumption.Repositories.Dtos
+{
+    public class RoomDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid Host { get; set; }
+        public Guid ActivePlaylistId { get; set; }
+        public int MaxViewersQuantity { get; set; }
+        public List<ViewerDto> Viewers { get; set; }
+    }
+}
---
+++ Mediaverse.Infrastructure/JointContentConsumption/Repositories/Dtos/ViewerDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Mediaverse.Infrastructure.JointContentConsumption.Repositories.Dtos
+{
+    public class ViewerDto
+    {
+        public Guid Id { get; set; }
+    }
+}
