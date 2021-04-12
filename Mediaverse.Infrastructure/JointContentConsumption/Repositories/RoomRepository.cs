4bfbbb3d2ab58f7df291f2bc49949a256e56cf66
floraldenis <floraldenis@gmail.com>
2021-04-12 19:45:22 +0300

    Finished implementing RoomRepository

--- Mediaverse.Infrastructure/JointContentConsumption/Repositories/RoomRepository.cs
+++ Mediaverse.Infrastructure/JointContentConsumption/Repositories/RoomRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Mediaverse.Domain.Authentication.Enums;
@@ -8,6 +9,7 @@ using Mediaverse.Domain.JointContentConsumption.Repositories;
 using Mediaverse.Domain.JointContentConsumption.ValueObjects;
 using Mediaverse.Infrastructure.Authentication.Repositories;
 using Mediaverse.Infrastructure.Common.Persistence;
+using Mediaverse.Infrastructure.JointContentConsumption.Repositories.Dtos;
 
 namespace Mediaverse.Infrastructure.JointContentConsumption.Repositories
 {
@@ -29,23 +31,66 @@ namespace Mediaverse.Infrastructure.JointContentConsumption.Repositories
         {
             var roomDto = await _applicationDbContext.Rooms.FindAsync(roomId);
 
-            var hostUser = await _userRepository.GetUserAsync(roomDto.Host, cancellationToken);
-            var host = new Viewer(
-                new UserProfile(hostUser.Id, hostUser.Nickname, hostUser.Type == UserType.Member));
+            var host = await GetViewer(roomDto.Host, cancellationToken);
+            var viewers = roomDto.Viewers
+                .Select(x => GetViewer(x.Id, cancellationToken))
+                .Select(t => t.Result)
+                .ToList();
             
-            var room = new Room(roomDto.Id, roomDto.Name, host);
-            
-            throw new NotImplementedException();
+            return new Room(
+                roomDto.Id,
+                roomDto.Name,
+                host, 
+                roomDto.MaxViewersQuantity,
+                roomDto.ActivePlaylistId,
+                viewers);
         }
 
-        public Task SaveAsync(Room room, CancellationToken cancellationToken)
+        public Task AddAsync(Room room, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var roomDto = new RoomDto
+            {
+                Id = room.Id,
+                Name = room.Name,
+                Host = room.Host.Profile.Id,
+                ActivePlaylistId = room.ActivePlaylistId,
+                MaxViewersQuantity = room.MaxViewersQuantity,
+                Viewers = room.Viewers
+                    .Select(v => new ViewerDto{ Id = v.Profile.Id })
+                    .ToList()
+            };
+
+            _applicationDbContext.Rooms.Add(roomDto);
+            
+            return _applicationDbContext.SaveChangesAsync(cancellationToken);
         }
 
-        public Task DeleteAsync(Room room, CancellationToken cancellationToken)
+        public Task UpdateAsync(Room room, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var roomDto = _applicationDbContext.Rooms.Find(room.Id);
+            roomDto.Name = room.Name;
+            roomDto.Host = room.Host.Profile.Id;
+            roomDto.ActivePlaylistId = room.ActivePlaylistId;
+            roomDto.MaxViewersQuantity = room.MaxViewersQuantity;
+            roomDto.Viewers = room.Viewers
+                .Select(v => new ViewerDto {Id = v.Profile.Id})
+                .ToList();
+
+            return _applicationDbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public Task DeleteAsync(Guid roomId, CancellationToken cancellationToken)
+        {
+            var roomDto = _applicationDbContext.Rooms.FindAsync(roomId);
+            _applicationDbContext.Remove(roomDto);
+
+            return _applicationDbContext.SaveChangesAsync(cancellationToken);
+        }
+        
+        private async Task<Viewer> GetViewer(Guid viewerId, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetUserAsync(viewerId, cancellationToken);
+            return new Viewer(new UserProfile(user.Id, user.Nickname, user.Type == UserType.Member));
         }
     }
 }
