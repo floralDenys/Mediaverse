7f5bed829d265af22fe1847c4fda9fba28fb9030
floraldenis <floraldenis@gmail.com>
2021-04-13 22:37:47 +0300

    Added Content to SearchResult. Updated naming in SearchResult

--- Mediaverse.Domain/ContentSearch/ValueObjects/SearchResult.cs
+++ Mediaverse.Domain/ContentSearch/ValueObjects/SearchResult.cs
@@ -5,6 +5,18 @@ namespace Mediaverse.Domain.ContentSearch.ValueObjects
 {
     public class SearchResult
     {
-        public IList<Preview> PreviewList { get; }
+        public IList<Preview> MatchingContentPreviews { get; }
+        
+        public Content RequestedContent { get; }
+
+        public SearchResult(IList<Preview> matchingContentPreviews)
+        {
+            MatchingContentPreviews = matchingContentPreviews;
+        }
+        
+        public SearchResult(Content requestedContent)
+        {
+            RequestedContent = requestedContent;
+        }
     }
 }
