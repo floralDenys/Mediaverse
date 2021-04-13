764d22b8c209a5509270ec513663579f0e373040
floraldenis <floraldenis@gmail.com>
2021-04-13 22:38:37 +0300

    Implemented ContentRepository

---
+++ Mediaverse.Infrastructure/JointContentConsumption/Repositories/ContentRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Mediaverse.Domain.JointContentConsumption.Entities;
+using Mediaverse.Domain.JointContentConsumption.Repositories;
+using Mediaverse.Domain.JointContentConsumption.ValueObjects;
+using Search = Mediaverse.Domain.ContentSearch;
+
+namespace Mediaverse.Infrastructure.JointContentConsumption.Repositories
+{
+    public class ContentRepository : IContentRepository
+    {
+        private readonly Search.Repositories.IContentRepository _contentRepository;
+
+        private readonly IMapper _mapper;
+
+        public ContentRepository(
+            Search.Repositories.IContentRepository contentRepository,
+            IMapper mapper)
+        {
+            _contentRepository = contentRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Content> GetAsync(ContentId contentId, CancellationToken cancellationToken)
+        {
+            var searchResult = await _contentRepository.SearchForContentAsync(
+                (Search.Enums.MediaContentSource) contentId.ContentSource,
+                Search.Enums.ContentQueryType.ContentId,
+                contentId.ExternalId,
+                cancellationToken);
+
+            return _mapper.Map<Content>(searchResult.RequestedContent);
+        }
+    }
+}
