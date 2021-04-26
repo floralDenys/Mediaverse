5d9ffba8d21f8f8f462260567528e28dcb7aed66
floraldenis <floraldenis@gmail.com>
2021-04-26 10:14:13 +0300

    Added InformativeException

---
+++ Mediaverse.Domain/Common/InformativeException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Mediaverse.Domain.Common
+{
+    public class InformativeException : InvalidOperationException
+    {
+        public InformativeException(string message, Exception innerException = null) : base(message, innerException)
+        { }
+    }
+}
