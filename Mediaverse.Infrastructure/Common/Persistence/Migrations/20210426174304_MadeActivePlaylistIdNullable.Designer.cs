05320c25f066f317506ca9ad201337f6ae9b98df
floraldenis <floraldenis@gmail.com>
2021-04-26 20:44:00 +0300

    Added migration

--- Mediaverse.Infrastructure/Common/Persistence/Migrations/20210426174304_MadeActivePlaylistIdNullable.Designer.cs
+++ Mediaverse.Infrastructure/Common/Persistence/Migrations/20210426174304_MadeActivePlaylistIdNullable.Designer.cs
@@ -4,14 +4,16 @@ using Mediaverse.Infrastructure.Common.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace Mediaverse.Infrastructure.Migrations
+namespace Mediaverse.Infrastructure.Common.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210426174304_MadeActivePlaylistIdNullable")]
+    partial class MadeActivePlaylistIdNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
